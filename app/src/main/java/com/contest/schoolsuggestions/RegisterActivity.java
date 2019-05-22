d27eeb23eda94689ab2745d91b24dfb80fb51a89
yerin <kimyerin125@gmail.com>
2019-05-23 02:38:18 +0900

    계정 생성 화면 디자인 - 토글

--- app/src/main/java/com/contest/schoolsuggestions/RegisterActivity.java
+++ app/src/main/java/com/contest/schoolsuggestions/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.contest.schoolsuggestions;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final TextView studentTextView = findViewById(R.id.studentText_register);
+        final TextView teacherTextView = findViewById(R.id.teacherText_register);
+        final LinearLayout studentInfoLayout = findViewById(R.id.studentInfoLayout_register);
+        final TextView codeTextView = findViewById(R.id.codeText_register);
+
+        studentTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                studentTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
+                teacherTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+                studentInfoLayout.setVisibility(View.VISIBLE);
+                codeTextView.setVisibility(View.GONE);
+            }
+        });
+        teacherTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                studentTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+                teacherTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
+                studentInfoLayout.setVisibility(View.GONE);
+                codeTextView.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
